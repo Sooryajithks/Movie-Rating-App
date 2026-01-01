d8e0ee02ec402ec02cd8933cb444f3560b49d61e
Sooryajith santhosh <sooryajithsanthosh@Sooryajiths-MacBook-Pro.local>
2026-01-01 20:35:51 +0800

    Render deployment prep

--- backend/settings.py
+++ backend/settings.py
@@ -29,5 +29,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = '/movies/'
+LOGIN_REDIRECT_URL = ''
 LOGOUT_REDIRECT_URL = '/accounts/login'
