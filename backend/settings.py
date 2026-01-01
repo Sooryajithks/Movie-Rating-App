6b7b67b9e41864e784d66d85f3588d8f3579062e
Sooryajith santhosh <sooryajithsanthosh@Sooryajiths-MacBook-Pro.local>
2026-01-01 19:39:14 +0800

    allowed hosts modified

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-dev-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['MMDB.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
