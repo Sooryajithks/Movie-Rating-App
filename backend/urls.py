c0007c408f0d5f3954f5b19cbea8a761d150cdad
Sooryajith santhosh <sooryajithsanthosh@Sooryajiths-MacBook-Pro.local>
2026-01-01 19:57:23 +0800

    edited the url path in url.py in the root

--- backend/urls.py
+++ backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('movies/', include('viewmovies.urls')),
+    path('', include('viewmovies.urls')),
 ]
