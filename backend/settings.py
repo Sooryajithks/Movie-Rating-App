c81cd1f2fa1a8ead02560da5573b89d51d16c640
Sooryajith santhosh <sooryajithsanthosh@Sooryajiths-MacBook-Pro.local>
2026-01-02 18:42:38 +0800

    final deployment after bug in login redirect

--- backend/settings.py
+++ backend/settings.py
@@ -135,7 +135,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Auth redirects
 # --------------------------------------------------
 LOGIN_URL = "/accounts/login/"
-LOGIN_REDIRECT_URL = "movielistpage"
+LOGIN_REDIRECT_URL = ""
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 # --------------------------------------------------
