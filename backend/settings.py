55592c712c78fe6cc99d0aa2dbb19244b2d94f33
Sooryajith santhosh <sooryajithsanthosh@Sooryajiths-MacBook-Pro.local>
2026-01-02 16:21:07 +0800

    Prepare for Railway deployment

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
